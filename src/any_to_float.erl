77f200b322afe944cab57806d76906911eb94a2e
Pawel Dudzinski <pawel.dudzinski@saucelabs.com>
2019-04-13 21:28:33 +0200

    Casting to float with tests

--- src/any_to_float.erl
+++ src/any_to_float.erl
@@ -1,1 +1,32 @@
 -module(any_to_float).
+
+-export([convert/1]).
+
+convert(Value) when is_float(Value) ->
+    {ok, Value};
+convert(Value) when is_integer(Value) ->
+    List = integer_to_list(Value),
+    {ok, list_to_float(List ++ ".0")};
+convert(Value) when is_binary(Value) ->
+    case cast:integer(Value) of
+        {ok, Integer} -> convert(Integer);
+        {error, cannot_cast} ->
+            try binary_to_float(Value) of
+                Integer -> {ok, Integer}
+            catch
+                error:_ -> {error, cannot_cast}
+            end
+    end;
+convert(Value) when is_list(Value) ->
+    TryIntFirst = try list_to_integer(Value) of
+        Integer -> integer_to_list(Integer) ++ ".0"
+    catch
+        error:_ -> Value
+    end,
+    try list_to_float(TryIntFirst) of
+        Float -> {ok, Float}
+    catch
+        error:_ -> {error, cannot_cast}
+    end;
+convert(_Value) ->
+    {error, cannot_cast}.
---
+++ test/any_to_float_test.erl
@@ -0,0 +1,21 @@
+-module(any_to_float_test).
+-include_lib("eunit/include/eunit.hrl").
+
+-define(F(X), cast:float(X)).
+
+float_to_float_test() ->
+    ?assertEqual(?F(10.0), {ok, 10.0}),
+    ?assertEqual(?F(-10.42), {ok, -10.42}).
+
+integer_to_float_test() ->
+    ?assertEqual(?F(10), {ok, 10.0}),
+    ?assertEqual(?F(-7), {ok, -7.0}).
+
+binary_to_float_test() ->
+    ?assertEqual(?F(<<"3">>), {ok, 3.0}),
+    ?assertEqual(?F(<<"-3">>), {ok, -3.0}).
+
+string_to_float_test() ->
+    ?assertEqual(?F("10.3"), {ok, 10.3}),
+    ?assertEqual(?F("20"), {ok, 20.0}),
+    ?assertEqual(?F("20a"), {error, cannot_cast}).
