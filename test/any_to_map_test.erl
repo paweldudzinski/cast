cb73fe41cb4c9eb20bb2840b7c7f98e37c6a2187
Pawel Dudzinski <pawel.dudzinski@saucelabs.com>
2019-04-15 15:41:13 +0200

    Any to map

---
+++ test/any_to_map_test.erl
@@ -0,0 +1,23 @@
+-module(any_to_map_test).
+-include_lib("eunit/include/eunit.hrl").
+
+-define(F(X), cast:map(X)).
+
+map_to_map_test() ->
+    Map = maps:put(test, 1, maps:new()),
+    ?assertEqual(cast:map(Map), {ok, #{test => 1}}).
+
+tuple_to_map_test() ->
+    Tuple = [{test, me}, {right, now}],
+    ?assertEqual(cast:map(Tuple), {ok, #{test => me, right => now}}).
+
+list_to_map_test() ->
+    Proplist = [{test, me}, {right, now}],
+    ?assertEqual(cast:map(Proplist), {ok, #{test => me, right => now}}).
+
+other_to_map_test() ->
+    ?assertEqual(cast:map(1), {error, cannot_cast}),
+    ?assertEqual(cast:map(1.0), {error, cannot_cast}),
+    ?assertEqual(cast:map("string"), {error, cannot_cast}),
+    ?assertEqual(cast:map(atom), {error, cannot_cast}),
+    ?assertEqual(cast:map(<<"bin">>), {error, cannot_cast}).
